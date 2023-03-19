1a68031116dc637d68d34744328dfd3ec77aad4b
Ricardsmm221RDB246 <125766591+Ricardsmm221RDB246@users.noreply.github.com>
2023-03-19 23:35:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 # python3
 
+import heapq
+
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    threads = [0] * n
+    job_queue = [(0, i) for i in range(m)]
+    heapq.heapify(job_queue)
+    while job_queue:
+        processing_time, job_index = heapq.heappop(job_queue)
+        thread_index = threads.index(min(threads))
+        start_time = threads[thread_index]
+        output.append((thread_index, start_time))
+        threads[thread_index] += processing_time
+        if job_queue:
+            next_processing_time, next_job_index = job_queue[0]
+            while next_job_index == job_index:
+                heapq.heappop(job_queue)
+                if job_queue:
+                    next_processing_time, next_job_index = job_queue[0]
+                else:
+                    break
     return output
 
+
+
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+    result = parallel_processing(n, m, data)
+    for thread_index, start_time in result:
+        print(thread_index, start_time)
+
     n = 0
     m = 0
 
