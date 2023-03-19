9406f2bc85fd31f4b1e1ee271c10277c64ff287b
Ricardsmm221RDB246 <125766591+Ricardsmm221RDB246@users.noreply.github.com>
2023-03-20 00:28:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,25 @@
-# python3
-
-import heapq
-
 import heapq
 
 def parallel_processing(n, m, data):
-    # create a list to store the output pairs
     output = []
+    threads = [(0, i) for i in range(n)]
+    heapq.heapify(threads)
 
-    # create a list to keep track of which threads are currently processing jobs
-    processing = [False] * n
-
-    for job_id, job_time in enumerate(data):
-        # find the next available thread
-        thread_id = next(i for i in range(n) if not processing[i])
-
-        # calculate the start time for the job
-        start_time = max(job_id, max(output[i][1] for i in range(len(output)) if output[i][0] == thread_id))
-
-        # update the processing list and add the output pair to the output list
-        processing[thread_id] = True
-        output.append((thread_id, start_time))
+    for job_index, job_time in enumerate(data):
+        start_time, thread_index = heapq.heappop(threads)
+        output.append((thread_index, start_time))
+        heapq.heappush(threads, (start_time + job_time, thread_index))
 
     return output
 
-
-
-
-
 def main():
-    # read input from the user
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
 
-    # call the parallel_processing function
     result = parallel_processing(n, m, data)
 
-    # print the output pairs
-    for thread_id, start_time in result:
-        print(thread_id, start_time)
-
-
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
+    for thread_index, start_time in result:
+        print(thread_index, start_time)
 
 if __name__ == "__main__":
     main()
