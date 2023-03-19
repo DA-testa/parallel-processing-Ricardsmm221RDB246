73c2ad7c882bfc79210a9731a4e95bcc25775909
Ricardsmm221RDB246 <125766591+Ricardsmm221RDB246@users.noreply.github.com>
2023-03-19 23:42:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,46 @@
 
 import heapq
 
+import heapq
+
 def parallel_processing(n, m, data):
+    # create a priority queue of (available_time, thread_id)
+    queue = [(0, i) for i in range(n)]
+
+    # create a list to store the output pairs
     output = []
-    threads = [0] * n
-    job_queue = [(0, i) for i in range(m)]
-    heapq.heapify(job_queue)
-    while job_queue:
-        processing_time, job_index = heapq.heappop(job_queue)
-        thread_index = threads.index(min(threads))
-        start_time = threads[thread_index]
-        output.append((thread_index, start_time))
-        threads[thread_index] += processing_time
-        if job_queue:
-            next_processing_time, next_job_index = job_queue[0]
-            while next_job_index == job_index:
-                heapq.heappop(job_queue)
-                if job_queue:
-                    next_processing_time, next_job_index = job_queue[0]
-                else:
-                    break
+
+    for job_id, job_time in enumerate(data):
+        # get the earliest available thread from the priority queue
+        available_time, thread_id = heapq.heappop(queue)
+
+        # calculate the time when the job will be completed
+        start_time = max(job_time, available_time)
+        finish_time = start_time + job_time
+
+        # add the output pair to the output list
+        output.append((thread_id, start_time))
+
+        # add the thread back to the priority queue with the updated available time
+        heapq.heappush(queue, (finish_time, thread_id))
+
     return output
 
 
 
+
 def main():
+    # read input from the user
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
+
+    # call the parallel_processing function
     result = parallel_processing(n, m, data)
-    for thread_index, start_time in result:
-        print(thread_index, start_time)
+
+    # print the output pairs
+    for thread_id, start_time in result:
+        print(thread_id, start_time)
+
 
     n = 0
     m = 0
